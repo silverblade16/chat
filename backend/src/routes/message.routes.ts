2902a3238125260a8624ad2715436df44611f2cd
Timur <m2205151@edu.misis.ru>
2024-12-22 18:46:37 +0300

    маршруты и контроллеры сообщений добавлены

--- backend/src/routes/message.routes.ts
+++ backend/src/routes/message.routes.ts
@@ -1,13 +1,13 @@
 import express from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import { getMessage, getUsersForSideBar, sendMessage } from "../controllers/message.controller.js";
 
 
 const router = express.Router();
 
-
-router.get("/conversations", (req, res)=>{
-    res.send("Conversation route");
-})
-
+router.get("/conversations", protectRoute as any, getUsersForSideBar as any);
+router.get("/:id",protectRoute as any, getMessage as any);
+router.post("/send/:id",protectRoute as any, sendMessage as any);
 
 export default router;
 
